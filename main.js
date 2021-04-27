fcb3470ed196f58c4b154cb4baa31b650328ba11
Noor Abdikadir <nooromar305@gmail.com>
2021-04-27 03:02:26 -0700

    already declared var removed

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ const figlet = require('figlet');
 const consoleTable = require('console.table') //Helps us display our application as a table in the console.
 
 //Custom console log text before application using Figlet.
-var figlet = require('figlet');
-
 figlet('Employee Management System', function (err, data) {
     if (err) {
         console.log('Something went wrong...');
