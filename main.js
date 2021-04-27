b34fa0b928d252ec681291db09c07c51cd92d8bc
Noor Abdikadir <nooromar305@gmail.com>
2021-04-27 02:18:26 -0700

    connection established

--- main.js
+++ main.js
@@ -6,11 +6,26 @@ const consoleTable = require('console.table') //Helps us display our application
 //Custom console log text before application using Figlet.
 var figlet = require('figlet');
  
-figlet('Hello World!!', function(err, data) {
+figlet('Employee Management System', function(err, data) {
     if (err) {
         console.log('Something went wrong...');
         console.dir(err);
         return;
     }
     console.log(data)
+});
+
+//mysql connection
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3301,
+    user: "root",
+    password: "", 
+    database: "employeeDB"
+});
+
+//check for error else start application
+connection.connect( ()=> (err) => {
+    if(err) throw err;
+    startApp();
 });
