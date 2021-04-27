0fc992c39e51e4938a5164699150a1699f5eee68
Noor Abdikadir <nooromar305@gmail.com>
2021-04-27 02:58:24 -0700

    updating

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const consoleTable = require('console.table') //Helps us display our application
 
 //Custom console log text before application using Figlet.
 var figlet = require('figlet');
- 
-figlet('Employee Management System', function(err, data) {
+
+figlet('Employee Management System', function (err, data) {
     if (err) {
         console.log('Something went wrong...');
         console.dir(err);
@@ -20,53 +20,57 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3301,
     user: "root",
-    password: "", 
+    password: "",
     database: "employeeDB"
 });
 
 //check for error else start application
-connection.connect( ()=> (err) => {
-    if(err) throw err;
+connection.connect(() => (err) => {
+    if (err) throw err;
     startApp();
 });
 
 function startApp() {
     inquirer
-    .prompt({
-        name: "action",
-        type: "rawlist",
-        message: "Hi there, what would you like to do?",
-        choices:[]
+        .prompt({
+            name: "action",
+            type: "rawlist",
+            message: "Hi there, what would you like to do?",
+            choices: []
+        })
+        .then(function (answer) {
+            switch (key) {
+                case value:
+
+                    break;
+
+                default:
+                    break;
+            }
+        })
+}
+
+
+function viewEmployeesByDepartment() {
+    var query = ` SELECT employees.id,employees.first_name, employees.last_name, role.title, departments.name AS department, role.salary, CONACT(manager.first_name, ' ', manager.last_name) AS Manager FROM employees LEFT JOIN role on employees.role_ID = role.id LEFT JOIN departments on role.department_ID = departments.id LEFT JOIN employees manager on manager.id = employees.manager_ID;`;
+    connection.query(query, function(err,query)) = ({
+
     })
-    .then(function(answer){
-        switch (key) {
-            case value:
-                
-                break;
-        
-            default:
-                break;
-        }
-    })
-}
-
-
-function viewEmployeesByDepartment(){
-    
-}
-
-function viewEmployees(){
 
 }
 
-function viewDepartment(){
-    
+function viewEmployees() {
+
 }
 
-function viewRoles(){
-    
+function viewDepartment() {
+
 }
 
-function addEmployee(){
+function viewRoles() {
+
+}
+
+function addEmployee() {
 
 }
