73b5275ce09197e2ba37a732f9706861b23ccdf9
Noor Abdikadir <nooromar305@gmail.com>
2021-04-27 00:08:01 -0700

    updated

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,9 @@
 /*caps not mandatory but helps with readability */
 DROP DATABASE IF EXISTS employeeDB; /*Never in real practice */
 CREATE DATABASE employeeDB;
+
+USE employeeDB;
+
+CREATE TABLE departments
+CREATE TABLE role
+CREATE TABLE employees
