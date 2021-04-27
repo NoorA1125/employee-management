c0138412a6dd02ae5a425044d79e73349f16baac
Noor Abdikadir <nooromar305@gmail.com>
2021-04-27 00:51:52 -0700

    updating role

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,11 +11,15 @@ INSERT INTO departments(name) VALUES
 
 
 /*Roles*/
-INSERT INTO departments(name) VALUES
-("Finance"),
-("Marketing"),
-("Operations"),
-("Engineering"),
+INSERT INTO role(title, salary, department_ID) VALUES
+("Accountant Manager", 100000, 1), --role, salary, employee count
+("Financial Associate", 62000,1),
+("Sales Manager", 90000, 2),
+("Sales Associate", 50000, 2),
+("HR")
+("HR")
+("HR")
+("HR")
 ("HR")
 
 
